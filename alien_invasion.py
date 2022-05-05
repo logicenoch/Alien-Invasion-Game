b76cd62df121b0c97c958835892b9d4321d8a663
Okeke Enoch Chibuzo <successenochokeke@gmail.com>
2022-05-05 02:40:58 +0100

    Settings.py added

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
-from turtle import bgcolor
 import pygame
+from settings import Settings
+
 
 def run_game():
     #initiate game and create the object screen.
@@ -8,22 +9,24 @@ def run_game():
     @init() initializes background settings that pygame
     needs to work properly.
     """
-
     pygame.init()
-    #screen is an object of the class "surface" 
+    ai_settings = Settings()
+    #screen is an object of the class "surface"
     #where you display a game element.
-    screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_width))
+
     pygame.display.set_caption("Alien Invasion By Logic")
-    bg_color = (230,230,230)
 
     #Start the main loop for the game
     while True:
         #Watch for keyboard and mouse event
         for event in pygame.event.get():
-            screen.fill(bg_color)
+            screen.fill(ai_settings.bg_color)
             if event.type == pygame.QUIT:
                 sys.exit()
         #Make the most recently drawn screen visible
         pygame.display.flip()
-        
+
+
 run_game()
